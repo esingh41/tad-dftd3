cc151048b7b0ea48cb98f06e3775e0cf1a778265
Elizabeth <esingh41@gatech.edu>
2025-10-12 13:32:03 -0400

    removed multiplying by half

--- src/tad_dftd3/disp.py
+++ src/tad_dftd3/disp.py
@@ -313,6 +313,7 @@ def dispersion2(
     #Adding so returns pairwise matrices and not sums, well it's all gonna be 23 by 23
     #Only way batching works, but I can apply a mask, to post_process
 
+    
     if mon_A_indices is not None and mon_B_indices is not None:
         e6 = -0.5 * torch.sum(c6 * t6, dim=-1)
         e8 = -0.5 * torch.sum(c8 * t8, dim=-1)        
@@ -323,10 +324,11 @@ def dispersion2(
     s6 = param.get("s6", torch.tensor(defaults.S6, **dd))
     s8 = param.get("s8", torch.tensor(defaults.S8, **dd))
 
+    #Not multiplying by 0.5 here because I adjusted the mask so it only returns AB
+    #interactions, and no BA interactions so no double counting so no need to multiply by 0.5
     if pairwise_matrix and mon_A_indices is not None and mon_B_indices is not None:
-        e6 = -0.5 * (c6 * t6) * s6
-        #print(e6)
-        e8 = -0.5 * (c8 * t8) * s8
+        e6 = (c6 * t6) * s6
+        e8 = (c8 * t8) * s8
         return e6 + e8, mask
     
     return s6 * e6 + s8 * e8
